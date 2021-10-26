0b69fbbd453bdfc0b0290d84d576e8b3c861b866
yang vieira <yangflix3@gmail.com>
2021-10-26 12:39:39 -0400

    lint 100%

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
 
 // O que será verificado:
@@ -8,30 +7,26 @@
 //     Verifica se após selecionar uma outra cor na paleta, é possível pintar os pixels com essa cor
 
 //     Verifica se somente o pixel que foi clicado foi preenchido com a cor selecionada, sem influenciar na cor dos demais pixels.
-let pixelart = document.querySelectorAll('.pixel')
-let colors = document.querySelectorAll('.color')
-let colorPai = document.querySelector('#pixel-board')
+const pixelart = document.querySelectorAll('.pixel');
+const colors = document.querySelectorAll('.color');
+const colorPai = document.querySelector('#pixel-board');
 
-for (let i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', function (e) {
-        for (let i = 0; i < colors.length; i++) {
-            colors[i].classList.remove('selected')
-        }
+for (let i = 0; i < colors.length; i += 1) {
+  colors[i].addEventListener('click', () => {
+    for (let ii = 0; ii < colors.length; ii += 1) {
+      colors[ii].classList.remove('selected');
+    }
 
-        colors[i].classList.add('selected')
-
-    });
+    colors[i].classList.add('selected');
+  });
 }
 
-
-colorPai.addEventListener('click', function (e) {
-    let selected = document.querySelector('.selected');
-    e.target.style.backgroundColor = selected.getAttribute('value');
+colorPai.addEventListener('click', (e) => {
+  const selected = document.querySelector('.selected');
+  e.target.style.backgroundColor = selected.getAttribute('value');
 });
 
-
-
-//O método getAttribute () retorna o valor do atributo com o nome especificado de um elemento.
+// O método getAttribute () retorna o valor do atributo com o nome especificado de um elemento.
 
 // 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
 
@@ -45,23 +40,9 @@ colorPai.addEventListener('click', function (e) {
 
 //     Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
 
-let bntLimpar = document.querySelector('#clear-board');
-bntLimpar.addEventListener('click',(e) =>{
- for (let index = 0; index < 25; index++) {
-     pixelart[index].style.backgroundColor = 'white'
-     
- }
-
+const bntLimpar = document.querySelector('#clear-board');
+bntLimpar.addEventListener('click', () => {
+  for (let index = 0; index < 25; index += 1) {
+    pixelart[index].style.backgroundColor = 'white';
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
