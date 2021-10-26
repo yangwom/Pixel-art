b582b1414e7b888e2961052c80b7de12cfc4ffc1
yang vieira <yangflix3@gmail.com>
2021-10-26 10:08:35 -0400

    commit da 8 finalizada

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
-let corSelecionada1 = document.querySelector('.black')
-let corSelecionada2 = document.querySelector('.green')
-let corSelecionada3 = document.querySelector('.blue')
-let corSelecionada4 = document.querySelector('.orange')
-let selected = document.querySelector('.selected')
-corSelecionada1.addEventListener('click', (e) => {
-    if (corSelecionada1.classList[2] !== 'selected') {
-        corSelecionada1.className += ' selected'
-        corSelecionada2.className = 'color green'
-        corSelecionada3.className = 'color blue'
-        corSelecionada4.className = 'color orange'
 
-    }
+// 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+
+// O que será verificado:
+
+//     Verifica se ao carregar a página deve ser possível pintar os pixels de preto
+
+//     Verifica se após selecionar uma outra cor na paleta, é possível pintar os pixels com essa cor
+
+//     Verifica se somente o pixel que foi clicado foi preenchido com a cor selecionada, sem influenciar na cor dos demais pixels.
+let pixelart = document.querySelectorAll('.pixel')
+let colors = document.querySelectorAll('.color')
+let colorPai = document.querySelector('#pixel-board')
+
+for (let i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', function (e) {
+        for (let i = 0; i < colors.length; i++) {
+            colors[i].classList.remove('selected')
+        }
+
+        colors[i].classList.add('selected')
+
+    });
+}
 
 
+colorPai.addEventListener('click', function (e) {
+    let selected = document.querySelector('.selected');
+    e.target.style.backgroundColor = selected.getAttribute('value');
 });
 
-corSelecionada2.addEventListener('click', (e) => {
-    if(corSelecionada2.classList[2] !== 'selected') {
-corSelecionada1.className = 'color black'
-    corSelecionada2.className += ' selected'
-    corSelecionada3.className = 'color blue'
-    corSelecionada4.className = 'color orange'
-    }
-    
-});
+//O método getAttribute () retorna o valor do atributo com o nome especificado de um elemento.
 
-corSelecionada3.addEventListener('click', (e) => {
-    if(corSelecionada3.classList[2] !== 'selected') {
-        corSelecionada1.className = 'color black'
-            corSelecionada2.className = 'color green'
-            corSelecionada3.className += ' selected'
-            corSelecionada4.className = 'color orange'
-            }
+// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
 
-});
+// O que será verificado:
 
-corSelecionada4.addEventListener('click', (e) => {
-e.target
-if(corSelecionada4.classList[2] !== 'selected') {
-    corSelecionada1.className = 'color black'
-        corSelecionada2.className = 'color green'
-        corSelecionada3.className = 'color blue'
-        corSelecionada4.className += ' selected'
-    }
+//     Verifica se o botão tem o id denominado clear-board
 
-});
+//     Verifica se o botão está posicionado entre a paleta de cores e o quadro de pixels
+
+//     Verifica se o texto do botão é 'Limpar'
+
+//     Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
+
+
+
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+
+// O que será verificado:
+
+//     Verifica se o botão tem o id denominado clear-board
+
+//     Verifica se o botão está posicionado entre a paleta de cores e o quadro de pixels
+
+//     Verifica se o texto do botão é 'Limpar'
+
+//     Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
